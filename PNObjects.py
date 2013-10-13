de16fcec2b6d09a27a39dc8ab01b6dc992e445ef
Mike Boyle <boyle@astro.cornell.edu>
2013-10-13 01:26:00 -0400

    Correct handling of positional and default arguments in view of args dictionary

--- PNObjects.py
+++ PNObjects.py
@@ -13,23 +13,23 @@ class PNSymbol(Symbol) :
     rest in `__init__`.
 
     """
-    def __new__(cls, name, constant, fundamental, substitution, atoms, datatype, **assumptions) :
+    def __new__(cls, name, constant, fundamental, substitution, substitution_atoms, datatype, **assumptions) :
         from sympy import Symbol
         return Symbol.__new__(cls, name, **assumptions)
-    def __init__(self, name, constant, fundamental, substitution, atoms, datatype, **kwargs) :
-        if not fundamental and isinstance(substitution, basestring) and not atoms:
+    def __init__(self, name, constant, fundamental, substitution, substitution_atoms, datatype, **kwargs) :
+        if not fundamental and isinstance(substitution, basestring) and not substitution_atoms:
             raise ValueError('Either `substitution` must be a sympy expression, '
-                             +'or `atoms` must be non-empty for derived variables.')
+                             +'or `substitution_atoms` must be non-empty for derived variables.')
         self.constant = constant
         self.fundamental = fundamental
         self.substitution = substitution
-        if atoms:
-            self.atoms = atoms
+        if substitution_atoms:
+            self.substitution_atoms = substitution_atoms
         else:
             try:
-                self.atoms = self.substitution.atoms(Symbol)
+                self.substitution_atoms = self.substitution.atoms(Symbol)
             except AttributeError:
-                self.atoms = None
+                self.substitution_atoms = None
         self.datatype = datatype
     def ccode(**args):
         from sympy import ccode, horner, N
@@ -58,24 +58,19 @@ class PNCollection(OrderedDict) : # subclass of OrderedDict
 
       * `constant` (boolean: doesn't need to be updated once it's defined)
       * `fundamental` (boolean: not defined in terms of other things)
-      * `substitutions` (string or sympy expression: re-express non-fundamental objects in terms of fundamentals)
-      * `atoms` (atomic sympy objects in terms of which the variable is defined)
+      * `substitution` (string or sympy expression: re-express non-fundamental object in terms of fundamentals)
+      * `substitution_atoms` (atomic sympy objects in terms of which the variable is defined)
       * `datatype` (optional name for datatype of variable in code output;
                     if None, the basic real datatype of the output language is assumed)
 
     """
     def __init__(self, *args):
         OrderedDict.__init__(self, *args)
-    def _AddVariable(self, name, constant=False, fundamental=False, substitution=None, atoms=None, datatype=None, **args) :
+    def _AddVariable(self, name, **args) :
         from inspect import currentframe
         from sympy import Basic, FunctionClass
         frame = currentframe().f_back.f_back
         try:
-            args['constant'] = constant
-            args['fundamental'] = fundamental
-            args['substitution'] = substitution
-            args['atoms'] = atoms
-            args['datatype'] = datatype
             sym = PNSymbol(name, **args)
             if sym is not None:
                 if isinstance(sym, Basic):
@@ -86,26 +81,42 @@ class PNCollection(OrderedDict) : # subclass of OrderedDict
             del frame
         self[sym] = name
         return sym
-    def AddVariable(self, name, constant=False, fundamental=False, substitution=None, atoms=None, datatype=None, **args) :
+    def AddVariable(self, name, constant=False, fundamental=False, substitution=None, substitution_atoms=None, datatype=None, **args) :
         return self._AddVariable(name, constant=constant, fundamental=fundamental,
-                                 substitution=substitution, atoms=atoms, datatype=datatype, **args)
+                                 substitution=substitution, substitution_atoms=substitution_atoms, datatype=datatype, **args)
     def AddBasicConstants(self, names, datatypes=None, **args) :
         from re import split
         names = split(',| ', names)
+        args['constant'] = True
+        args['fundamental'] = True
+        args['substitution'] = args.pop('substitution', None)
+        args['substitution_atoms'] = args.pop('substitution_atoms', None)
+        args['datatype'] = args.pop('datatype', None)
         for name in names :
             if name :
-                self._AddVariable(name, constant=True, fundamental=True, substitution=None,
-                                  atoms=None, datatype=datatypes, **args)
-    def AddBasicVariables(self, names, datatypes=None, **args) :
+                self._AddVariable(name, **args)
+    def AddBasicVariables(self, names, **args) :
         from re import split
         names = split(',| ', names)
+        args['constant'] = False
+        args['fundamental'] = True
+        args['substitution'] = args.pop('substitution', None)
+        args['substitution_atoms'] = args.pop('substitution_atoms', None)
+        args['datatype'] = args.pop('datatype', None)
         for name in names :
             if name :
-                self._AddVariable(name, constant=False, fundamental=True, substitution=None,
-                                  atoms=None, datatype=datatypes, **args)
-    def AddDerivedConstant(self, name, substitution, atoms=None, datatype=None, **args) :
-        self._AddVariable(name, constant=True, fundamental=False, substitution=substitution,
-                          atoms=atoms, datatype=datatype, **args)
-    def AddDerivedVariable(self, name, substitution, atoms=None, datatype=None, **args) :
-        self._AddVariable(name, constant=False, fundamental=False, substitution=substitution,
-                          atoms=atoms, datatype=datatype, **args)
+                self._AddVariable(name, **args)
+    def AddDerivedConstant(self, name, substitution, **args) :
+        args['constant'] = True
+        args['fundamental'] = False
+        args['substitution'] = substitution
+        args['substitution_atoms'] = args.pop('substitution_atoms', None)
+        args['datatype'] = args.pop('datatype', None)
+        self._AddVariable(name, **args)
+    def AddDerivedVariable(self, name, substitution, **args) :
+        args['constant'] = False
+        args['fundamental'] = False
+        args['substitution'] = substitution
+        args['substitution_atoms'] = args.pop('substitution_atoms', None)
+        args['datatype'] = args.pop('datatype', None)
+        self._AddVariable(name, **args)
