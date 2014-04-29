7e5332aaaf9d024f0755943efb8ee42d683fbaa4
Mike Boyle <boyle@astro.cornell.edu>
2014-04-29 07:30:17 -0400

    Add Iij.. interpretations

---
+++ Waveforms/results/IijklCMS.py
@@ -0,0 +1,4 @@
+( ( ( 24*nu*(3*nu - 1)*r(t)**3*v(t)/(5*c**3) )*lambdaHat*nHat*nHat*nHattimesSVec )
+ +( ( 24*delta*nu*(2*nu - 1)*r(t)**3*v(t)/(5*c**3) )*lambdaHat*nHat*nHat*nHattimesSigmaVec )
+ +( ( 4*delta*nu*(21*nu - 8)*r(t)**3*v(t)/(5*c**3) )*nHat*nHat*nHat*SigmaVectimeslambdaHat )
+ +( ( 32*nu*(3*nu - 1)*r(t)**3*v(t)/(5*c**3) )*nHat*nHat*nHat*SVectimeslambdaHat ) )
---
+++ Waveforms/results/JijklCMS.py
@@ -0,0 +1,2 @@
+( ( ( nu*(15*nu/2 - 5/2)*r(t)**3/c )*nHat*nHat*nHat*SigmaVec )
+ +( ( -5*delta*nu*r(t)**3/(2*c) )*nHat*nHat*nHat*S ) )
